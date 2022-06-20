b827a7bfd44b8143bdb33dba6868bac7e45c6bc3
javier <javipsalm@gmail.com>
2022-06-20 05:35:18 +0200

    Actualizacion del codigo ConsultarPedido

--- ConsultarPedido.java
+++ ConsultarPedido.java
@@ -3,8 +3,29 @@ package buscadorPedidos;
 import java.util.HashMap;
 import java.util.Set;
 
+/**
+ * Clase que crea un diccionario almacenado en la variable pedidos
+ * @version 0.8
+ * @author zuper
+ *
+ */
+
 public class ConsultarPedido {
+	
+	/**
+	 * Variable de clase; Es un diccionario de todos los pedidos realizados.
+	 */
+	
   public static HashMap<String, String> pedidos = new HashMap<String, String>();
+  
+  /**
+   * Metodo buscarPedido; Comprueba si un identificador esta en los pedidos
+   * y te muestra su contenido o si existe.
+   * @param identificador; Es un string que tiene cada pedido que sirve para comprobar su
+   * estado
+   * @return Devuelve el estado del pedido
+   * @see ConsultarPedido crea el diccionario donde se almacenan los identificadores
+   */
 
   public static String buscarPedido(String identificador) {
 	    Set<String> identificadores = pedidos.keySet();
